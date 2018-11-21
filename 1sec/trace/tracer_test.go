9508f5034128685c870662e60ce9ab9d312f7f0a
yusuke ohno <y.ono.varial@gmail.com>
2018-11-21 22:19:52 +0900

    fix tracer_test

--- 1sec/trace/tracer_test.go
+++ 1sec/trace/tracer_test.go
@@ -2,8 +2,32 @@ package trace
 
 import(
 	"testing"
+	"bytes"
+	"io"
+	"fmt"
 )
 
+type tracer struct {
+	out io.Writer
+}
+func (t *tracer) Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
 func TestNew(t *testing.T) {
-	t.Error("まだテスト作成してない")
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Newからの戻り値がnilです")
+	} else {
+		tracer.Trace("こんにちは、traceパッケージ")
+		if buf.String() != "こんにちは、traceパッケージ\n" {
+			t.Errorf("'%s'という誤った文字列が出力されました", buf.String())
+		}
+	}
 }
