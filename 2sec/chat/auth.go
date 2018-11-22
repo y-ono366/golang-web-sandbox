a9bdca0af60156dffb060e5b667ed1edb461691e
yusuke ohno <y.ono.varial@gmail.com>
2018-11-22 11:50:03 +0900

    merge auth

---
+++ 2sec/chat/auth.go
@@ -0,0 +1,27 @@
+package main
+
+import(
+	"net/http"
+)
+
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
+		// 未認証
+		w.Header().Set("Location","/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else if err != nil {
+		//other error
+		panic(err.Error())
+	} else {
+		// success
+		h.next.ServeHTTP(w,r)
+	}
+}
+
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next:handler}
+}
